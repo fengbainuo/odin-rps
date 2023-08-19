05ff72f70da8ec86299dbc28b6588887edf5dc72
bainuo <97295550+fengbainuo@users.noreply.github.com>
2023-08-19 09:05:03 +0200

    assignment finished, see the README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rps
+
+This is the basic, console-based version of the Odin Project's rock-paper-scissors javaScript assignment. The player input prompt is case insensitive, so don't worry about the capitalization. Tested and works as long as the player input is valid
--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let options = ["rock", "paper", "scissors"];
+const options = ["rock", "paper", "scissors"];
 
-let getComputerChoice = () => Math.floor(Math.random() * options.length);
+const getComputerChoice = () => options[Math.floor(Math.random() * options.length)];
 
-console.log(getComputerChoice);
+const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+
+const choices = [playerSelection, getComputerChoice()];
+
+function whoWins(x, y) {
+    if (x == 0) {
+        return `You win, ${y[0]} beats ${y[1]}!`;
+    } else {
+        return `You lose, ${y[1]} beats ${y[0]}!`;
+    }
+}
+
+function playRound(choices) {
+    if (choices[0] == choices[1]) {
+        return "Draw!";
+    } else if (choices.includes("rock") && choices.includes("paper")) {
+        const winner = choices.indexOf("paper");
+        return whoWins(winner, choices);
+    } else if (choices.includes("paper") && choices.includes("scissors")) {
+        const winner = choices.indexOf("scissors");
+        return whoWins(winner, choices);
+    } else {
+        const winner = choices.indexOf("rock");
+        return whoWins(winner, choices);
+    }
+}
+
+console.log(playRound(choices));
